da46e3f790d897e9d0843a1975443a98b886e4ea
luckmer <woszczo04@gmail.com>
2021-07-08 10:20:29 +0200

    viedo app main folder

---
+++ src/Views/VideoApp.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import { NavBar } from "../Components";
+
+const VideoApp = () => {
+  return (
+    <Fragment>
+      <NavBar />
+    </Fragment>
+  );
+};
+
+export default VideoApp;
