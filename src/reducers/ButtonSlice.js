90f3f26bef6800f4898ad944e637673ab1c73794
luckmer <woszczo04@gmail.com>
2021-07-08 17:39:38 +0200

    control buttons data

--- src/reducers/ButtonSlice.js
+++ src/reducers/ButtonSlice.js
@@ -1,15 +1,50 @@
 import { createSlice } from "@reduxjs/toolkit";
-
 const initialState = {
-  type: "",
+  type: "tile",
+  filterType: {
+    favorite: "",
+    data: "",
+    row: "",
+  },
 };
 
 const ButtonSlice = createSlice({
   name: "buttonPanel",
   initialState,
   reducers: {
-    setClickType: (state, action) => {},
+    setClickType: (state, action) => {
+      const { view } = action.payload;
+      state.type !== view ? (state.type = view) : (state.type = "tile");
+    },
+
+    SetFilterType: (state, action) => {
+      const { type, color } = action.payload;
+      const copy = state.filterType;
+
+      if (type === "favorite") {
+        state.filterType = {
+          ...copy,
+          favorite: type === "favorite" && color === "danger" ? "" : type,
+        };
+      }
+
+      if (type === "new") {
+        state.filterType = {
+          ...copy,
+          data: type === "new" && color === "danger" ? "" : type,
+        };
+      }
+
+      if (type === "row") {
+        state.filterType = {
+          ...copy,
+          row: type === "row" && color === "danger" ? "" : type,
+        };
+      }
+    },
   },
 });
+
+export const { SetFilterType, setClickType } = ButtonSlice.actions;
 
 export default ButtonSlice.reducer;
