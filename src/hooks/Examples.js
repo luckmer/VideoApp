ac6b08185a9ddb999ef2bb89311dfe6ad8ea9e8e
luckmer <woszczo04@gmail.com>
2021-07-08 11:14:41 +0200

    movies

---
+++ src/hooks/Examples.js
@@ -0,0 +1,11 @@
+const Demo = () => [
+  "https://vimeo.com/181696349",
+  "https://vimeo.com/407943692",
+  "http://vimeo.com/423630",
+  "https://vimeo.com/1399176",
+  "http://vimeo.com/423630087",
+  "https://www.youtube.com/watch?v=JEuAYnjtJP0",
+  "https://www.youtube.com/watch?v=4JOAqRS_lms",
+];
+
+export default Demo;
