0e0dc57eeca9d8ba91c0d4ef5cae9a34e43d411a
luckmer <woszczo04@gmail.com>
2021-07-08 17:39:55 +0200

    index update

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from "redux";
 import MovieSlice from "./MovieSlice";
+import ButtonSlice from "./ButtonSlice";
+import PaginationSlice from "./PaginationSlice";
 
 const Index = combineReducers({
   movie: MovieSlice,
+  buttonPanel: ButtonSlice,
+  PaginationSlice,
 });
 
 export default Index;
