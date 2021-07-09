0970c27622f4dee9d5e7252ae78870268d30e534
luckmer <woszczo04@gmail.com>
2021-07-09 10:48:39 +0200

    api html panel

--- src/Components/ApiFetch/ApiHtml.js
+++ src/Components/ApiFetch/ApiHtml.js
@@ -1,6 +1,7 @@
 import CalcDate from "./CalcDate";
-
 import {
+  ListGroup,
+  ListGroupItem,
   Card,
   CardText,
   CardBody,
@@ -11,10 +12,63 @@ import {
   CardImg,
 } from "reactstrap";
 
-const Tile = (i, el, handleSave, LikeColor, handleDelete) => {
+const RowView = ({
+  i,
+  CustomBody,
+  handleSetupModal,
+  el,
+  handleSave,
+  LikeColor,
+  handleDelete,
+}) => {
+  return (
+    <ListGroup key={i}>
+      <ListGroupItem>
+        <CardBody style={CustomBody}>
+          <CardTitle
+            tag="h5"
+            onClick={() => handleSetupModal(el)}
+            style={{ cursor: "pointer" }}
+          >
+            {el.title || el.name}
+          </CardTitle>
+          <CardSubtitle tag="h6" className="mb-2 text-muted">
+            <CalcDate date={el.published} />
+          </CardSubtitle>
+          <CardText>likes {el.likes}</CardText>
+          <CardText>Views: {el.viewCount | 0}</CardText>
+          <ButtonGroup>
+            <Button href={el.link} color="primary">
+              Play
+            </Button>
+            <Button
+              onClick={() => handleSave(el.id, LikeColor)}
+              color={LikeColor}
+            >
+              Save
+            </Button>
+            <Button color="primary" onClick={() => handleDelete(el.id)}>
+              Delete
+            </Button>
+          </ButtonGroup>
+        </CardBody>
+      </ListGroupItem>
+    </ListGroup>
+  );
+};
+
+const TileView = ({
+  i,
+  handleSetupModal,
+  el,
+  handleSave,
+  LikeColor,
+  handleDelete,
+}) => {
   return (
     <Card style={{ width: "21rem" }} key={i}>
       <CardImg
+        onClick={() => handleSetupModal(el)}
         top
         style={{ cursor: "pointer" }}
         src={el.pictures}
@@ -46,4 +100,4 @@ const Tile = (i, el, handleSave, LikeColor, handleDelete) => {
   );
 };
 
-export { Tile };
+export { TileView, RowView };
