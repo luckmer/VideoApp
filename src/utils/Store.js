6d6b9077ae183714501446f56362a8512a27969a
luckmer <woszczo04@gmail.com>
2021-07-09 10:51:10 +0200

    new reduxSlice

--- src/utils/Store.js
+++ src/utils/Store.js
@@ -1,6 +1,30 @@
-import { createStore } from "redux";
-import Index from "../reducers";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunkMiddleware from "redux-thunk";
+import Index from "../reducers/index";
 
-const Store = createStore(Index);
+const Save = (state) => {
+  try {
+    const data = JSON.stringify(state);
+    localStorage.setItem("CONTEXT", data);
+  } catch (err) {
+    throw err;
+  }
+};
+
+const Load = () => {
+  try {
+    const data = localStorage.getItem("CONTEXT");
+    if (data === null) return;
+    return JSON.parse(data);
+  } catch (err) {
+    throw err;
+  }
+};
+
+const middleware = composeWithDevTools(applyMiddleware(thunkMiddleware));
+const Store = createStore(Index, Load(), middleware);
+
+Store.subscribe(() => Save(Store.getState()));
 
 export default Store;
